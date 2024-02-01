0f2fc2e7d3877e1d5a0bd7ebd2893a9a12cfaa23
sergio-alv-per <55792768+sergio-alv-per@users.noreply.github.com>
2024-02-01 18:19:32 +0100

    Formatting.

--- what2watch-frontend/src/App.jsx
+++ what2watch-frontend/src/App.jsx
@@ -1,17 +1,30 @@
-import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate, useParams } from 'react-router-dom'
-import { usePopularFilms } from './hooks'
+import { useState } from "react"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+  useParams,
+} from "react-router-dom"
+import { usePopularFilms } from "./hooks"
 
 function App() {
-  const [userID, setUserID] = useState('')
+  const [userID, setUserID] = useState("")
   const [websocket, setWebSocket] = useState(null)
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home setUserID={setUserID} setWebSocket={setWebSocket} />}/>
-        <Route path="/rooms/:roomID" element={<Room userID={userID} websocket={websocket} />} />
-        <Route path="*" element={<Navigate to="/"/>} />
+        <Route
+          path="/"
+          element={<Home setUserID={setUserID} setWebSocket={setWebSocket} />}
+        />
+        <Route
+          path="/rooms/:roomID"
+          element={<Room userID={userID} websocket={websocket} />}
+        />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   )
@@ -21,7 +34,9 @@ function Home({ setUserID, setWebSocket }) {
   const navigate = useNavigate()
 
   const createWebSocket = (roomID, userID) => {
-    const ws = new WebSocket(`ws://localhost:8000/rooms/${roomID}/users/${userID}/ws`)
+    const ws = new WebSocket(
+      `ws://localhost:8000/rooms/${roomID}/users/${userID}/ws`
+    )
 
     ws.onopen = () => {
       console.log(`WS: ${userID} connected to room ${roomID}`)
@@ -36,7 +51,7 @@ function Home({ setUserID, setWebSocket }) {
   }
 
   const createUserAndWS = (roomID) => {
-    fetch(`http://localhost:8000/rooms/${roomID}/users`, { method: 'POST' })
+    fetch(`http://localhost:8000/rooms/${roomID}/users`, { method: "POST" })
       .then((res) => res.json())
       .then((data) => {
         setUserID(data.id)
@@ -46,54 +61,63 @@ function Home({ setUserID, setWebSocket }) {
   }
 
   const createRoomAndUserAndWS = () => {
-    fetch('http://localhost:8000/rooms', { method: 'POST' })
-    .then((res) => res.json())
-    .then((data) => data.id)
-    .then(
-      (roomID) => {
+    fetch("http://localhost:8000/rooms", { method: "POST" })
+      .then((res) => res.json())
+      .then((data) => data.id)
+      .then((roomID) => {
         createUserAndWS(roomID)
         navigate(`/rooms/${roomID}`)
-      }
-    )
-    .catch((err) => console.log(err))
+      })
+      .catch((err) => console.log(err))
   }
 
   const connectToRoom = (evt) => {
     evt.preventDefault()
     const roomID = evt.target.roomID.value
-    
+
     createUserAndWS(roomID)
 
     navigate(`/rooms/${roomID}`)
   }
 
   return (
-  <div className="bg-slate-600 flex flex-col space-y-5 justify-center items-center p-4 min-h-screen">
-    <h1 className="text-white font-bold font-mono text-5xl">What2Watch</h1>
-    <div className="flex justify-center items-center h-64 w-64">
-      <p className='text-white'> placeholder carousel </p>
+    <div className="bg-slate-600 flex flex-col space-y-5 justify-center items-center p-4 min-h-screen">
+      <h1 className="text-white font-bold font-mono text-5xl">What2Watch</h1>
+      <div className="flex justify-center items-center h-64 w-64">
+        <p className="text-white"> placeholder carousel </p>
+      </div>
+      <Button onClick={createRoomAndUserAndWS}>Create room</Button>
+      <p className="text-white">or</p>
+      <form
+        className="flex flex-col space-y-5 justify-center items-center"
+        onSubmit={connectToRoom}
+      >
+        <input
+          className="rounded-md border-2 border-teal-500 bg-slate-600 text-white text-xl font-bold px-2 py-2"
+          type="text"
+          name="roomID"
+          placeholder="Room ID"
+        />
+        <Button type="submit">Connect to room</Button>
+      </form>
     </div>
-    <Button onClick={createRoomAndUserAndWS}>Create room</Button>
-    <p className='text-white'>or</p>
-    <form className="flex flex-col space-y-5 justify-center items-center" onSubmit={connectToRoom}>
-      <input className="rounded-md border-2 border-teal-500 bg-slate-600 text-white text-xl font-bold px-2 py-2" type="text" name="roomID" placeholder="Room ID" />
-      <Button type="submit">Connect to room</Button>
-    </form>
-  </div>
   )
 }
 
 function Button({ children, disabled, ...props }) {
   const enabledStyles = "bg-teal-200 rounded-md text-xl font-bold px-5 py-2"
-  const disabledStyles = "bg-teal-200 rounded-md text-xl font-bold px-5 py-2 opacity-50 cursor-not-allowed"
-  
+  const disabledStyles =
+    "bg-teal-200 rounded-md text-xl font-bold px-5 py-2 opacity-50 cursor-not-allowed"
+
   return (
-    <button className={disabled ? disabledStyles : enabledStyles} {...props}>{children}</button>
+    <button className={disabled ? disabledStyles : enabledStyles} {...props}>
+      {children}
+    </button>
   )
 }
 
 function Room({ userID, websocket }) {
-  const [recievedMessage, setRecievedMessage] = useState('')
+  const [recievedMessage, setRecievedMessage] = useState("")
   const { roomID } = useParams()
 
   if (websocket) {
@@ -104,22 +128,22 @@ function Room({ userID, websocket }) {
 
   return (
     <div className="bg-slate-600 flex flex-col space-y-5 justify-center items-center p-4 min-h-screen">
-      <p className="text-white font-bold text-xl" >{roomID}</p>
+      <p className="text-white font-bold text-xl">{roomID}</p>
 
-      {userID && websocket
-      ? (
+      {userID && websocket ? (
         <>
           <FilmSwiper roomID={roomID} userID={userID} />
 
           <div>
-            <button onClick={() => websocket.send(`${userID} says hello!`)}>Send message</button>
+            <button onClick={() => websocket.send(`${userID} says hello!`)}>
+              Send message
+            </button>
             <p>{recievedMessage}</p>
           </div>
         </>
-      )
-      : <p>Loading...</p>
-      }
-
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   )
 }
@@ -128,14 +152,12 @@ function FilmSwiper({ roomID, userID }) {
   const [shownFilmSet, setShownFilmSet] = useState(0)
   const [page0, setPage0] = useState(1)
   const [page1, setPage1] = useState(2)
-  const { films: films0, loading: loading0, error: error0 } = usePopularFilms({ page: page0 })
-  const { films: films1, loading: loading1, error: error1 } = usePopularFilms({ page: page1 })
+  const { films: films0, loading: loading0 } = usePopularFilms({ page: page0 })
+  const { films: films1, loading: loading1 } = usePopularFilms({ page: page1 })
   const [shownFilmIndex, setShownFilmIndex] = useState(0)
 
   const films = shownFilmSet === 0 ? films0 : films1
   const loading = shownFilmSet === 0 ? loading0 : loading1
-  const error = shownFilmSet === 0 ? error0 : error1
-  const page = shownFilmSet === 0 ? page0 : page1
 
   const handleNext = () => {
     const nextShownFilmIndex = shownFilmIndex + 1
@@ -157,9 +179,13 @@ function FilmSwiper({ roomID, userID }) {
 
   const sendSwipe = (liked) => {
     const body = JSON.stringify({ film_id: films[shownFilmIndex].id, liked })
-    const headers = { 'Content-Type': 'application/json' }
+    const headers = { "Content-Type": "application/json" }
 
-    fetch(`http://localhost:8000/rooms/${roomID}/users/${userID}/swipes`, {method: 'POST', body, headers})
+    fetch(`http://localhost:8000/rooms/${roomID}/users/${userID}/swipes`, {
+      method: "POST",
+      body,
+      headers,
+    })
   }
 
   const handleLike = () => {
@@ -175,24 +201,37 @@ function FilmSwiper({ roomID, userID }) {
   return (
     <>
       <div>
-        {
-          loading
-          ? <p>Loading...</p>
-          : (
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
           <div className="flex flex-col space-y-5 justify-center items-center">
-            <img className="rounded-md shadow-md h-80" src={films[shownFilmIndex].poster} alt={films[shownFilmIndex].name} />
-            <p className="text-white font-bold text-xl" >{films[shownFilmIndex].name}</p>
+            <img
+              className="rounded-md shadow-md h-80"
+              src={films[shownFilmIndex].poster}
+              alt={films[shownFilmIndex].name}
+            />
+            <p className="text-white font-bold text-xl">
+              {films[shownFilmIndex].name}
+            </p>
           </div>
-          )
-        }
+        )}
       </div>
       <div className="flex justify-center items-center space-x-5">
-        <button className="bg-red-200 rounded-md text-xl font-bold px-5 py-2" onClick={handleDislike}>Dislike</button>
-        <button className="bg-teal-200 rounded-md text-xl font-bold px-5 py-2" onClick={handleLike}>Like</button>
+        <button
+          className="bg-red-200 rounded-md text-xl font-bold px-5 py-2"
+          onClick={handleDislike}
+        >
+          Dislike
+        </button>
+        <button
+          className="bg-teal-200 rounded-md text-xl font-bold px-5 py-2"
+          onClick={handleLike}
+        >
+          Like
+        </button>
       </div>
     </>
   )
 }
-
 
 export default App
--- what2watch-frontend/src/hooks.jsx
+++ what2watch-frontend/src/hooks.jsx
@@ -1,24 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
 export function usePopularFilms({ page = 1 }) {
-    const [films, setFilms] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [films, setFilms] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
-    useEffect(() => {
-        setLoading(true)
-        setError(null)
-        fetch(`http://localhost:8000/films?page=${page}`)
-            .then(res => res.json())
-            .then(data => {
-                setFilms(data)
-                setLoading(false)
-            })
-            .catch(err => {
-                setError(err)
-                setLoading(false)
-            })
-    }, [page])
+  useEffect(() => {
+    setLoading(true)
+    setError(null)
+    fetch(`http://localhost:8000/films?page=${page}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setFilms(data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        setError(err)
+        setLoading(false)
+      })
+  }, [page])
 
-    return { films, loading, error }
+  return { films, loading, error }
 }
