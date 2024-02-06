8052b68751f4e9c34ad5c0d420415afe161dcd56
sergio-alv-per <55792768+sergio-alv-per@users.noreply.github.com>
2024-02-06 18:05:33 +0100

    Fixed 404 page look.

--- what2watch-frontend/src/Page404.jsx
+++ what2watch-frontend/src/Page404.jsx
@@ -18,19 +18,21 @@ export function Page404() {
   }, [navigate, timeLeft])
 
   return (
-    <div className="bg-slate-600 flex flex-col gap-3 items-center p-4 min-h-screen">
-      <h1 className="text-white font-bold font-mono text-5xl top">
-        What2Watch
-      </h1>
-      <div className="flex flex-col gap-3 justify-center items-center max-w-md">
-        <h2 className="text-white font-bold text-3xl">404 - Page not found</h2>
-        <p className="text-white">
+    <div className="flex justify-center">
+      <div className="flex flex-col gap-3 items-center min-h-screen max-w-xl pb-5">
+        <Link to="/">
+          <img src="./logo.svg" alt="What2Watch logo" className="p-5 h-24" />
+        </Link>
+        <h1 className="text-gray-800 font-bold text-3xl">
+          404 - Page not found
+        </h1>
+        <p className="text-gray-800">
           The page you are looking for does not exist.{" "}
           <Link to="/" className="underline font-bold">
             Go back to the home page
           </Link>
         </p>
-        <p className="text-white">
+        <p className="text-gray-800">
           You will be redirected to the home page in {timeLeft} seconds
         </p>
       </div>
